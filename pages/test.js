4ceac4fe243c3600daf8a4540f6c3324c2b54e7d
msyukurrr <msyukur@my.bcit.ca>
2022-04-04 12:17:37 -0700

    button done

---
+++ pages/test.js
@@ -0,0 +1,14 @@
+import Button from "../comps/button.js";
+
+export default function test(){
+    return <div>
+        <h1>Test</h1>
+        <Button 
+        labeltxt='enter'
+        />
+        <Button 
+        labeltxt='mambojambo'
+        bg='yellow'
+        />
+    </div>
+}
