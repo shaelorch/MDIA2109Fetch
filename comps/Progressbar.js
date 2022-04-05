3d9821f222acfe945917dc8c549119010b089e3e
shaelorch <slorch@my.bcit.ca>
2022-04-05 13:14:44 -0700

    first commit

---
+++ comps/Progressbar.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const ProgCont = styled.div`
+
+`;
+
+const Progressbar = styled.img`
+
+`;
+
+
+export default function Progress({
+    
+}){
+    return 
+    <ProgCont />
+}
