dbd352f2ca16763d57791e372d37df4c0980a9b2
marisalen <mlenarduzzi1@my.bcit.ca>
2022-04-07 17:51:04 -0700

    added circle header comp

---
+++ comps/CircleHeader.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+const CircleCont = styled.div`
+border-radius: 50%;
+width: 80px;
+height: 80px;
+padding: 4px;
+
+background: #F8D2CF;
+color: #EBB2AD;
+text-align: center;
+
+font: 72px 'Passion One', cursive;
+`
+
+const CircleText = styled.div`
+color: #EBB2AD;
+left:20px;
+`
+
+export default function Input({
+  labeltxt='#',
+}){
+
+  return <CircleCont>
+    {labeltxt}
+  </CircleCont>
+}
