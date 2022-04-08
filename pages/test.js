a8c16a29c1d4bba67903a7097981d98e71c6f5b3
soleildagneau <sdagneau@my.bcit.ca>
2022-04-07 17:54:25 -0700

    hamburger component (wrong branch oops)

--- pages/test.js
+++ pages/test.js
@@ -1,32 +1,17 @@
 import Input from '../comps/Input';
 import Error from '../comps/Error';
 import Header from '../comps/Header';
+import Hamburger from '../comps/Hamburger';
 
 
 export default function test(){
   return <div>
     <h1>Test</h1>
-    <Input 
-      labeltxt='Email'
-    />
-    <Input 
-      labeltxt='Password'
-      inputtype='password'
-    />
-    <Input
-      labeltxt='Important !'
-      inputtype='number'
-      bg='pink'
-    />
-
-    <Error
-        errortxt='message'
-        bg='pink'
-        />
-
+  
     <Header
         headertext='header'
         size='48px'
     />
+    <Hamburger />
   </div>
 }
