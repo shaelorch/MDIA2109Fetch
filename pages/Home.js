e338e7a3cc6e48c769b90b752db1942c5b204344
shaelorch <slorch@my.bcit.ca>
2022-04-12 12:23:31 -0700

    added home page and got rid of test.js

---
+++ pages/Home.js
@@ -0,0 +1,10 @@
+import Header from "../comps/Header";
+
+export default function Home(){
+    return <div>
+        <Header 
+            headertext="WELCOME TO"
+        />
+        <
+    </div>
+}
