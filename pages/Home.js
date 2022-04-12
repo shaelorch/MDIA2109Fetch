eb2605f5d823f25564bb208808ebe8e0f3c93abc
shaelorch <slorch@my.bcit.ca>
2022-04-12 12:52:50 -0700

    added hamburger

--- pages/Home.js
+++ pages/Home.js
@@ -1,14 +1,31 @@
 import Header from "../comps/Header";
 
 import Logo from "../comps/Logo";
+
 import Paragraph from "../comps/Paragraph";
+
+import MainButton from "../comps/mainButton";
+
+import Hamburger from "../comps/Hamburger";
 
 export default function Home(){
     return <div>
+        <Hamburger />
         <Header 
             headertext="WELCOME TO"
         />
         <Logo />
         <Paragraph labeltxt="Fetch is a web app that connects you with your perfect dog from a rescue center!"/>
+        <MainButton 
+        labeltxt="Tutorial" 
+        bg="#EBB2AD" 
+        border="#ffffff" 
+        color="#EBB2AD"/>
+        <MainButton 
+        labeltxt="Find a pup!" 
+        bg="#EBB2AD" 
+        border="#ffffff" 
+        color="#EBB2AD"/>
+
     </div>
 }
