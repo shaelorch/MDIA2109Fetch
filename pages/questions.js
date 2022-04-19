716a7ec6cdc9991d1270fff2fe3211fcee713e61
msyukurrr <msyukur@my.bcit.ca>
2022-04-19 11:10:53 -0700

    questions page

--- pages/questions.js
+++ pages/questions.js
@@ -1,3 +1,4 @@
+import styled from 'styled-components';
 import TopBar from '../comps/TopBar';
 import Header from '../comps/Header';
 import MainButton from '../comps/MainButton';
@@ -9,23 +10,33 @@ import NextandBack from '../comps/NextandBack';
 import { qs } from '../comps/data/que_content';
 import { useRouter } from 'next/router';
 
+
+const BackgroundDiv = styled.div `
+background-image: linear-gradient(#D6EEFF, #D6EEC9)`
+
+const ButtonCont = styled.div `
+
+`
 export default function Questions(){
 
-  return <div>
+  return <BackgroundDiv>
     <TopBar />
-    <Progress />
-    <Header
+    <Progress />  
+      <Header
        headertext='1. Occupation'
        size='50px'/>
+
     <Paragraph
         labeltxt='What type of job do you have?' />
 
+    <ButtonCont>
     <MainButton
           labeltxt='Part-time'
           bg='#D6EEC9'
           color='#7EA172'
           border='5px solid #7EA172'
           ts = 'none' />
+    </ButtonCont>
     <MainButton
           labeltxt='Full-time'
           bg='#D6EEC9'
@@ -34,6 +45,6 @@ export default function Questions(){
           ts = 'none' />
     <NextandBack />
 
-  </div>
+  </BackgroundDiv>
 }
 
