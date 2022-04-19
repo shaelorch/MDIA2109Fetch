0a75de49f77e6b81e13bdd0e177913286693e9ba
msyukurrr <msyukur@my.bcit.ca>
2022-04-19 10:18:10 -0700

    data

---
+++ comps/data/que_content.js
@@ -0,0 +1,11 @@
+export const qs = [
+    {
+        title:"how are you doing?",
+        options:["good","bad","okay"]
+    },
+
+    {
+        title:"what are you doing?",
+        options:["bing","bong"]
+    }
+]
--- pages/questions.js
+++ pages/questions.js
@@ -6,10 +6,16 @@ import Paragraph from '../comps/Paragraph';
 import Logo from '../comps/Logo';
 import Progress from '../comps/Progressbar';
 import NextandBack from '../comps/NextandBack';
+import { qs } from '../comps/data/que_content';
+import { useRouter } from 'next/router';
 
+export default function Questions(){
 
+  var {qnum} = r.query;
+  if(qnum === undefined){
+    qnum = 0;
+  }
 
-export default function test(){
   return <div>
     <TopBar />
     <Progress />
@@ -35,3 +41,4 @@ export default function test(){
 
   </div>
 }
+
