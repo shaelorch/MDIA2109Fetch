0b772de80d8f1b49069ac6f86c6a0eb16b89c798
soleildagneau <sdagneau@my.bcit.ca>
2022-04-26 13:17:51 -0700

    next and back button fixed

--- comps/NextandBack.js
+++ comps/NextandBack.js
@@ -7,16 +7,31 @@ display:flex;
 justify-content: space-between;
 margin-top: 20px;
 `
+const Left = styled.div`
+position: fixed;
+bottom: 50px;
+left: 50px;
+`
+const Right = styled.div`
+position: fixed;
+bottom: 50px;
+right: 50px;
+`
+
 export default function NextandBack({
     onClickNext=()=>{},
     onClickBack=()=>{}
 }){
     return <CirButtonCont>
+        <Left>
         <CircleButtonLeft onClick = {onClickBack}>
 
         </CircleButtonLeft>
+        </Left>
+        <Right>
         <CircleButtonRight onClick = {onClickNext}>
 
         </CircleButtonRight>
+        </Right>
     </CirButtonCont>
 }
