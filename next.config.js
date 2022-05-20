10f68fc62770aed45272d1619ee9b99590f476db
shaelorch <slorch@my.bcit.ca>
2022-05-19 22:31:50 -0700

    fixing errors

--- next.config.js
+++ next.config.js
@@ -6,10 +6,12 @@ const next = {
   }
 }
 
+module.exports = next
+
 module.exports = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-} 
+}
